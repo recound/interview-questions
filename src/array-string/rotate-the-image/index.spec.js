f36366d613dfeddd838bfb06f29c1d6c85781124
Kirill Klochkov <recound5@gmail.com>
2021-05-15 12:30:42 +0500

    feat: add more test casese

--- src/array-string/rotate-the-image/index.spec.js
+++ src/array-string/rotate-the-image/index.spec.js
@@ -2,12 +2,14 @@ const { rotateTheImage } = require('./index');
 
 describe('rotateTheImage tests', () => {
     it('General cases', () => {
+        expect(rotateTheImage([[1]])).toStrictEqual([[1]])
         expect(rotateTheImage([
             [1,2,3,4],
             [5,6,7,8],
             [9,10,11,12],
             [13,14,15,16]
-        ])).toStrictEqual([ [ 13, 9, 5, 1 ],
+        ])).toStrictEqual([
+            [ 13, 9, 5, 1 ],
             [ 14, 10, 6, 2 ],
             [ 15, 11, 7, 3 ],
             [ 16, 12, 8, 4 ] ])
