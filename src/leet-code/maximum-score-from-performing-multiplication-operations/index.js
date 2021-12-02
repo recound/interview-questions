d93713984cf19be4b468d570795a7a73267512ab
Kirill Klochkov <recound5@gmail.com>
2021-12-02 19:14:39 +0500

    fix

--- src/leet-code/maximum-score-from-performing-multiplication-operations/index.js
+++ src/leet-code/maximum-score-from-performing-multiplication-operations/index.js
@@ -4,20 +4,20 @@
  * @return {number}
  */
 var maximumScore = function(nums, multipliers) {
-    const memo = {};
+    const memo = Array.from({ length: multipliers.length }, () => Array.from({ length: multipliers.length }));
     function dp(left, i) {
         if (multipliers.length === i) {
             return 0;
         }
-        if (memo[`${left},${i}`] !== undefined) {
-            return memo[`${left},${i}`];
+        if (memo[left][i] !== undefined) {
+            return memo[left][i];
         }
         const currentM = multipliers[i];
-        memo[`${left},${i}`] = Math.max(
+        memo[left][i] = Math.max(
             nums[left] * currentM + dp(left + 1, i + 1),
             nums[nums.length - 1 - (i - left)] * currentM + dp(left, i + 1)
         );
-        return memo[`${left},${i}`];
+        return memo[left][i];
     }
     return dp(0, 0);
 };
